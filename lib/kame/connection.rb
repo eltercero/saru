558022bcc55e67032b473e74295ef5d4a9fe1cca
Victor <victor.martin84@gmail.com>
2017-04-20 12:47:47 +0200

    Module functions

--- lib/kame/connection.rb
+++ lib/kame/connection.rb
@@ -7,5 +7,21 @@ require 'kame/connection/user'
 module Kame
   module Connection
 
+    def user level=nil
+      User.new(level).call
+    end
+
+    def radical level=nil
+      Radical.new(level).call
+    end
+
+    def kanji level=nil
+      Kanji.new(level).call
+    end
+
+    def vocabulary level=nil
+      Vocabulary.new(level).call
+    end
+
   end
 end
